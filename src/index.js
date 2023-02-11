9e093ffafdf7077ea41f8bcbe0d7c458888213fe
GeorgyGGVe <patriotge@mail.ru>
2023-02-12 01:12:32 +0300

    feat: function is created

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  for(let i=0; i < bracketsConfig.length; i++) {
+    //console.log( bracketsConfig[i].join('') ); 
+  if(str.indexOf(bracketsConfig[i].join('')) > -1){
+          while(str.indexOf(bracketsConfig[i].join('')) > -1){
+          str = str.replace(bracketsConfig[i].join('') , '');
+          i = 0;
+          } 
+  }
+} 
+if(str == ''){
+  return true;
+} else return false;
 }
